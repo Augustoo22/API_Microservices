309dc41ab14355113f28b0fff70905e33779e468
Augustoo22 <victoraugustodocarmo32@gmail.com>
2024-12-31 16:46:15 -0300

    feat foi criado API CLientes para o inicio da arquitetura de microservico

---
+++ backend/cliente/src/main/java/com/api/cliente/model/Cliente.java
@@ -0,0 +1,72 @@
+package com.api.cliente.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private int quantidadeVeiculos;
+
+    // Construtor padrão (sem argumentos)
+    public Cliente() {
+    }
+
+    // Construtor com todos os campos
+    public Cliente(Long id, String nome, String cpf, String dataNascimento, int quantidadeVeiculos) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.quantidadeVeiculos = quantidadeVeiculos;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getQuantidadeVeiculos() {
+        return quantidadeVeiculos;
+    }
+
+    public void setQuantidadeVeiculos(int quantidadeVeiculos) {
+        this.quantidadeVeiculos = quantidadeVeiculos;
+    }
+}
---
+++ backend/cliente/src/main/java/com/api/cliente/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.api.cliente.repository;
+
+import com.api.cliente.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
