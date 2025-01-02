4ee65c1d9acee42f5c0c82e03f9d23046bcc0c7c
Augustoo22 <victoraugustodocarmo32@gmail.com>
2025-01-02 17:00:20 -0300

    feat: criado API de carros MVC na porta 7070

---
+++ backend/veiculo/src/main/java/com/api/veiculo/repository/VeiculoRepository.java
@@ -0,0 +1,9 @@
+package com.api.veiculo.repository;
+
+import com.api.veiculo.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long>{ 
+}
