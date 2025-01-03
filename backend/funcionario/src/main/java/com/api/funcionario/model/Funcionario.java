a5758dc171577e7741ccb92ad1a4bab70a0c6670
Augustoo22 <victoraugustodocarmo32@gmail.com>
2025-01-03 16:31:29 -0300

    feat: API de funcionario construida usando MVC na porta 5050

---
+++ backend/funcionario/src/main/java/com/api/funcionario/model/Funcionario.java
@@ -0,0 +1,75 @@
+package com.api.funcionario.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_funcionario")
+public class Funcionario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String cargo;
+    private String dataNascimento;
+    private String especialidade;
+    private int anosExperiencia;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public int getAnosExperiencia() {
+        return anosExperiencia;
+    }
+
+    public void setAnosExperiencia(int anosExperiencia) {
+        this.anosExperiencia = anosExperiencia;
+    }
+}
---
+++ backend/funcionario/src/main/java/com/api/funcionario/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.api.funcionario.repository;
+
+import com.api.funcionario.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+}
